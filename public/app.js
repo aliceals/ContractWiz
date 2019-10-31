c390a0555ced429b9fbc562a1213d16866452ce8
Alice Alsford <alice.alsford@gmail.com>
2019-10-31 15:58:24 +1300

    drop down for booking page

---
+++ public/app.js
@@ -0,0 +1,11 @@
+function apiFetch() {
+    fetch("API/getweather")
+        .then(response => {
+            return response.json()
+        })
+        .then(json => {
+            console.log(json)
+        })
+}
+
+apiFetch()
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 // const db = require('../db')
 const router = express.Router()
 const hbs = require('express-handlebars')
+const fetch = require('node-fetch')
+
 
 router.get('/', (req, res) => {
     res.redirect('/home')
@@ -10,5 +12,25 @@ router.get('/', (req, res) => {
 router.get('/home', (req, res) => {
     res.render('home')
 })
+
+router.get('/book', (req, res) => {
+    res.render('book')
+})
+
+
+
+
+
+
+
+
+// router.get("API/getweather", (req, res) => {
+//     fetch("https://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7")
+//         .then((res) => res.json())
+//         .then(json => {
+//             console.log(json)
+//             res.send(json)
+//         })
+// })
 
 module.exports = router
