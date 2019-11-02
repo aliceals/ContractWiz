c31994a663e7856c19b13c7809cddab251c47b57
Alice Alsford <alice.alsford@gmail.com>
2019-11-02 16:04:42 +1300

    connected weather api v2

--- routes/routes.js
+++ routes/routes.js
@@ -12,11 +12,23 @@ router.get('/', (req, res) => {
     res.redirect('/home')
 })
 
+
 router.get('/home', (req, res) => {
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=Wellington,nzl&APPID=ba4745edb1c9e697fb3063cbb511b2bd")
+    fetch("https://api.darksky.net/forecast/6fe0e60f51867939f3313dd1351dcd17/-41.131489,174.839996")
         .then((res) => res.json())
         .then(json => {
-            let weather = json.list[0].weather[0].main
+            console.log(json.daily.data[1].icon)
+
+            let weatherTom = json.daily.data[1].icon
+            let weatherTwo = json.daily.data[2].icon
+            let weatherThree = json.daily.data[3].icon
+            let weatherFour = json.daily.data[4].icon
+            let weatherFive = json.daily.data[5].icon
+            let weatherSix = json.daily.data[6].icon
+            let weatherSeven = json.daily.data[7].icon
+
+
+
             let now = moment().subtract(10, 'days').format('MMM Do YY')
             let tomorrow = moment().add(1, 'days').format('MMM Do YY')
             let twodays = moment().add(2, 'days').format('MMM Do YY')
@@ -27,30 +39,65 @@ router.get('/home', (req, res) => {
             let sevendays = moment().add(7, 'days').format('MMM Do YY')
 
             let data = {
-                tomorrow: tomorrow,
-                twodays: twodays,
-                threedays: threedays,
-                fourdays: fourdays,
-                fivedays: fivedays,
-                sixdays: sixdays,
-                sevendays: sevendays,
-                weather: weather
+                dates: { tomorrow, twodays, threedays, fourdays, fivedays, sixdays, sevendays },
+                weather: {
+                    weatherTom, weatherTwo, weatherThree, weatherFour, weatherFive, weatherSix, weatherSeven
+                },
+                // temp: { maxTempTom }
             }
 
             res.render('home', data)
         })
 
-
-
-
-    // let pageModel = {
-    //     dates =[tomorrow, twodays, threedays, fourdays, fivedays, sixdays, sevendays]
-    // }
-
-
-
-
 })
+
+
+
+
+// router.get('/home', (req, res) => {
+//     fetch("http://api.openweathermap.org/data/2.5/forecast?q=Wellington,nzl&units=metric&APPID=ba4745edb1c9e697fb3063cbb511b2bd")
+//         .then((res) => res.json())
+//         .then(json => {
+//             let weatherTom = json.list[1].weather[0].main
+//             let weatherTwo = json.list[2].weather[0].main
+//             let weatherThree = json.list[3].weather[0].main
+//             let weatherFour = json.list[4].weather[0].main
+//             let weatherFive = json.list[5].weather[0].main
+//             let weatherSix = json.list[0].weather[0].main
+//             let weatherSeven = json.list[1].weather[0].main
+//             let maxTempTom = json.list[1].main.temp_max
+
+//             let now = moment().subtract(10, 'days').format('MMM Do YY')
+//             let tomorrow = moment().add(1, 'days').format('MMM Do YY')
+//             let twodays = moment().add(2, 'days').format('MMM Do YY')
+//             let threedays = moment().add(3, 'days').format('MMM Do YY')
+//             let fourdays = moment().add(4, 'days').format('MMM Do YY')
+//             let fivedays = moment().add(5, 'days').format('MMM Do YY')
+//             let sixdays = moment().add(6, 'days').format('MMM Do YY')
+//             let sevendays = moment().add(7, 'days').format('MMM Do YY')
+
+//             let data = {
+//                 dates: { tomorrow, twodays, threedays, fourdays, fivedays, sixdays, sevendays },
+//                 weather: {
+//                     weatherTom, weatherTwo, weatherThree, weatherFour, weatherFive, weatherSix, weatherSeven
+//                 },
+//                 temp: { maxTempTom }
+//             }
+
+//             res.render('home', data)
+//         })
+
+
+
+
+//     // let pageModel = {
+//     //     dates =[tomorrow, twodays, threedays, fourdays, fivedays, sixdays, sevendays]
+//     // }
+
+
+
+
+// })
 
 router.post('/home', (req, res) => {
     console.log(req.body)
