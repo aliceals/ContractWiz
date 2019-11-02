60825c0522ffec912aa7bacbbabb219fcca44eb3
Alice Alsford <alice.alsford@gmail.com>
2019-11-02 16:22:56 +1300

    added max and min temp

--- routes/routes.js
+++ routes/routes.js
@@ -17,7 +17,7 @@ router.get('/home', (req, res) => {
     fetch("https://api.darksky.net/forecast/6fe0e60f51867939f3313dd1351dcd17/-41.131489,174.839996")
         .then((res) => res.json())
         .then(json => {
-            console.log(json.daily.data[1].icon)
+            console.log(json.daily.data[1])
 
             let weatherTom = json.daily.data[1].icon
             let weatherTwo = json.daily.data[2].icon
@@ -27,7 +27,21 @@ router.get('/home', (req, res) => {
             let weatherSix = json.daily.data[6].icon
             let weatherSeven = json.daily.data[7].icon
 
+            let tempTomHigh = json.daily.data[1].temperatureHigh
+            let tempTwoHigh = json.daily.data[2].temperatureHigh
+            let tempThreeHigh = json.daily.data[3].temperatureHigh
+            let tempFourHigh = json.daily.data[4].temperatureHigh
+            let tempFiveHigh = json.daily.data[5].temperatureHigh
+            let tempSixHigh = json.daily.data[6].temperatureHigh
+            let tempSevenHigh = json.daily.data[7].temperatureHigh
 
+            let tempTomLow = json.daily.data[1].temperatureLow
+            let tempTwoLow = json.daily.data[2].temperatureLow
+            let tempThreeLow = json.daily.data[3].temperatureLow
+            let tempFourLow = json.daily.data[4].temperatureLow
+            let tempFiveLow = json.daily.data[5].temperatureLow
+            let tempSixLow = json.daily.data[6].temperatureLow
+            let tempSevenLow = json.daily.data[7].temperatureLow
 
             let now = moment().subtract(10, 'days').format('MMM Do YY')
             let tomorrow = moment().add(1, 'days').format('MMM Do YY')
@@ -43,7 +57,10 @@ router.get('/home', (req, res) => {
                 weather: {
                     weatherTom, weatherTwo, weatherThree, weatherFour, weatherFive, weatherSix, weatherSeven
                 },
-                // temp: { maxTempTom }
+                temp: {
+                    tempTomHigh, tempTwoHigh, tempThreeHigh, tempFourHigh, tempFiveHigh, tempSixHigh, tempSevenHigh,
+                    tempTomLow, tempTwoLow, tempThreeLow, tempFourLow, tempFiveLow, tempSixLow, tempSevenLow
+                }
             }
 
             res.render('home', data)
@@ -51,53 +68,6 @@ router.get('/home', (req, res) => {
 
 })
 
-
-
-
-// router.get('/home', (req, res) => {
-//     fetch("http://api.openweathermap.org/data/2.5/forecast?q=Wellington,nzl&units=metric&APPID=ba4745edb1c9e697fb3063cbb511b2bd")
-//         .then((res) => res.json())
-//         .then(json => {
-//             let weatherTom = json.list[1].weather[0].main
-//             let weatherTwo = json.list[2].weather[0].main
-//             let weatherThree = json.list[3].weather[0].main
-//             let weatherFour = json.list[4].weather[0].main
-//             let weatherFive = json.list[5].weather[0].main
-//             let weatherSix = json.list[0].weather[0].main
-//             let weatherSeven = json.list[1].weather[0].main
-//             let maxTempTom = json.list[1].main.temp_max
-
-//             let now = moment().subtract(10, 'days').format('MMM Do YY')
-//             let tomorrow = moment().add(1, 'days').format('MMM Do YY')
-//             let twodays = moment().add(2, 'days').format('MMM Do YY')
-//             let threedays = moment().add(3, 'days').format('MMM Do YY')
-//             let fourdays = moment().add(4, 'days').format('MMM Do YY')
-//             let fivedays = moment().add(5, 'days').format('MMM Do YY')
-//             let sixdays = moment().add(6, 'days').format('MMM Do YY')
-//             let sevendays = moment().add(7, 'days').format('MMM Do YY')
-
-//             let data = {
-//                 dates: { tomorrow, twodays, threedays, fourdays, fivedays, sixdays, sevendays },
-//                 weather: {
-//                     weatherTom, weatherTwo, weatherThree, weatherFour, weatherFive, weatherSix, weatherSeven
-//                 },
-//                 temp: { maxTempTom }
-//             }
-
-//             res.render('home', data)
-//         })
-
-
-
-
-//     // let pageModel = {
-//     //     dates =[tomorrow, twodays, threedays, fourdays, fivedays, sixdays, sevendays]
-//     // }
-
-
-
-
-// })
 
 router.post('/home', (req, res) => {
     console.log(req.body)
